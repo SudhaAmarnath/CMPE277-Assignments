a89a9987c4c890b91c10eb8a529515a80e2fcd1c
SudhaAmarnath <sudha04.a@gmail.com>
2019-05-20 20:24:00 -0700

    new changes

--- iosFileStorage/FileStorageAndArchive/ViewController.swift
+++ iosFileStorage/FileStorageAndArchive/ViewController.swift
@@ -7,7 +7,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
          self.view.backgroundColor = UIColor(red: 90/255, green: 200/255, blue: 250/255, alpha: 1)
-        // Do any additional setup after loading the view, typically from a nib.
     }
     @IBOutlet weak var AuthorName: UITextField!
 
@@ -23,21 +22,17 @@ class ViewController: UIViewController {
         return documentsDir
     }
     @IBAction func file(_ sender: Any) {
-        let documentDirURL = getDocDirectory() // create the destination url for the text file to be saved
-        let fileDestUrl = documentDirURL.appendingPathComponent("Assigment_FileManager.txt")
-        print(fileDestUrl.path) //print file path
+        let documentDirURL = getDocDirectory()
+        let fileDestUrl = documentDirURL.appendingPathComponent("FileStorageAndArchive.txt")
+        print(fileDestUrl.path)
         let text = generateString()
         
         do {
-            // writing to disk
             try text.write(to: fileDestUrl, atomically: false, encoding: String.Encoding.utf8)
             
-            // saving was successful. any code posterior code goes here
-            
-            // reading from disk
             do {
                 let mytext = try String(contentsOf: fileDestUrl, encoding: String.Encoding.utf8)
-                print(mytext)   // "some text\n"
+                print(mytext)
             } catch let error as NSError {
                 print("error loading from url \(String(describing: fileDestUrl))")
                 print(error.localizedDescription)
@@ -51,14 +46,13 @@ class ViewController: UIViewController {
     @IBAction func storage(_ sender: Any) {
         let text = generateString()
         let data = NSKeyedArchiver.archivedData(withRootObject: text)
-        let fullPath = getDocDirectory().appendingPathComponent("Assigment_Archive.dat")
+        let fullPath = getDocDirectory().appendingPathComponent("FileStorageAndArchive.ar")
         print(fullPath.path)
         do {
             try data.write(to: fullPath)
         } catch {
             print("Could not write the file")
         }
-        
         
         if let loadedStrings = NSKeyedUnarchiver.unarchiveObject(withFile: fullPath.path) {
             print("read back from the archives: \(loadedStrings)")
@@ -68,7 +62,6 @@ class ViewController: UIViewController {
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
